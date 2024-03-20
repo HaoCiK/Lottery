a6bef8f5dc5047b6e2711d940b3ba92147307dba
HC.K <haocikoay@gmail.com>
2024-03-20 21:16:09 +0800

    Push files

---
+++ compile.js
@@ -0,0 +1,9 @@
+// compile code will go here
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+
+module.exports = solc.compile(source, 1).contracts[':Lottery']; //1 is number of contracts we want to deploy
---
+++ deploy.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { Web3 } = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const mneumonic = process.env.mneumonic;
+const testnet_api = process.env.testnet_api;
+
+const provider = new HDWalletProvider(
+    mneumonic,
+    testnet_api
+);
+const web3 = new Web3(provider);
+//updated web3 and hdwallet-provider imports added for convenience
+
+// deploy code will go here
+// the main reason we put it in deploy is because we want make our life easier with async
+const deploy = async () => {
+    accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: ['Welcome to Web3!'] })
+        .send({ from: accounts[0], gas: 1000000 })
+
+    console.log('Contract deployed to', result.options.address);
+};
+deploy();
