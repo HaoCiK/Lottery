2584f3c4b4a076f621d31651789a7b46cfe53702
HC.K <haocikoay@gmail.com>
2024-03-22 22:41:31 +0800

    added react frontend code

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -21,9 +21,10 @@ const deploy = async () => {
     console.log('Attempting to deploy from account', accounts[0]);
 
     const result = await new web3.eth.Contract(JSON.parse(interface))
-        .deploy({ data: bytecode, arguments: ['Welcome to Web3!'] })
+        .deploy({ data: bytecode })
         .send({ from: accounts[0], gas: 1000000 })
 
     console.log('Contract deployed to', result.options.address);
+    console.log(interface);
 };
 deploy();
---
+++ react-frontend/lottery-react/src/web3.js
@@ -0,0 +1,22 @@
+import Web3 from 'web3';
+
+let web3;
+
+if (window.ethereum) {
+    web3 = new Web3(window.ethereum);
+    try {
+        // Request account access if needed
+        window.ethereum.request({ method: 'eth_requestAccounts' });
+    } catch (error) {
+        // User denied account access...
+        console.error("User denied account access")
+    }
+} else if (window.web3) {
+    // Legacy dapp browsers...
+    web3 = new Web3(window.web3.currentProvider);
+} else {
+    // Non-dapp browsers...
+    console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+}
+
+export default web3;
